652424df82285ef45bf927c4d14a4ce027161e0c
KidDosz99 <hazysah02@raudah.usim.edu.my>
2025-03-18 10:28:17 +0800

    Remove unnecessary files

--- script.js
+++
@@ -1,8 +0,0 @@
-function checkFlag() {
-    let input = prompt("Enter the flag:");
-    if (input === "CTF{example_flag}") {
-        alert("Correct!");
-    } else {
-        alert("Wrong flag!");
-    }
-}
