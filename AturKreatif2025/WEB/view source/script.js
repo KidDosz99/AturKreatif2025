89d8d65121811036ffdc1d4ee467d62b7c67b6eb
Mohammad Hazwan Syahmi Bin Mohd Rizman <hazysah02@raudah.usim.edu.my>
2025-03-24 13:34:47 +0800

    Remove robots.txt chal and view source challenges

--- AturKreatif2025/WEB/view source/script.js
+++
@@ -1,9 +0,0 @@
-// Event listener untuk butang hint
-document.getElementById("hintButton").addEventListener("click", function() {
-    let hint = document.getElementById("hint");
-    if (hint.classList.contains("hidden")) {
-        hint.classList.remove("hidden");
-    } else {
-        hint.classList.add("hidden");
-    }
-});
